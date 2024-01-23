db78063a41b78fc849ee1e64bcb9e74358652f6f
Bruno Fukumori <brunofukumori@gmail.com>
2024-01-23 11:20:23 -0300

    chore: adjustments in assets and config files

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,9 @@
-import { PrismaClient } from "@prisma/client";
 import { books } from "./constants/books";
 import { categories } from "./constants/categories";
 import { ratings } from "./constants/ratings";
 import { users } from "./constants/users";
+import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
 async function main() {
